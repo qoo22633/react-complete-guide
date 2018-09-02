2935a3b9ed5115dbdceb8421f311b0b03aa97117
yudai yamashita <yama226.33@gmail.com>
2018-09-02 12:50:47 +0900

    Persons、Personコンポーネントをステートフルに変更 #83

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Person.css';
 
-const person =(props)=> {
-
-   return(
-       <div className={classes.Person}>
-        <p onClick={props.click}>I'm a {props.name} and I'm {props.age} years old</p> 
-        <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name}></input>
+class Person extends Component {
+    render () {
+       return <div className={classes.Person}>
+        <p onClick={this.props.click}>I'm a {this.props.name} and I'm {this.props.age} years old</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name}></input>
        </div>
-   )
- };
+    }
+}
 
-export default person;
+export default Person;
